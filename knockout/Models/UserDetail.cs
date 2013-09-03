3fdc29ab776e44a7e0fd9713ba846cb77912a801
vivekkumarm <mvivekkumar89@karunya.edu.in>
2013-09-03 16:03:33 +0530

    Knockout CRUD Operation

---
+++ knockout/Models/UserDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace knockout.Models
+{
+    public class UserDetail
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Bdate { get; set; }
+    }
+}
---
+++ knockout/ViewModel/UserDetail.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    ko.applyBindings(new customerVM(), document.getElementById('displayNode'));
+    ko.applyBindings(new customer(), document.getElementById('createNode'));
+});
+
+function customer(name, age, comments) {
+    var self = this;
+
+
+    self.Name = name;
+    self.Age = age,
+        self.Comments = comments;
+
+    self.addCustomer = function () {
+        alert("ok");
+        //$.ajax({
+        //    url: "/api/customer/",
+        //    type: 'post',
+        //    data: ko.toJSON(this),
+        //    contentType: 'application/json',
+        //    success: function (result) {
+        //    }
+        //});
+    }
+}
